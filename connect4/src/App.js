510bd24f67b4d72d296319ff18a97650770a8f5f
Amit Vemuri <136983+amitV@users.noreply.github.com>
2025-02-28 23:18:29 -0500

    connect4
    deploy

---
+++ connect4/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Connect4 from './Connect4';
+
+function App() {
+  return (
+    <div className="App">
+      <Connect4 />
+    </div>
+  );
+}
+
+export default App;
