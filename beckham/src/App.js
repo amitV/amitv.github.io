5cbebdd246187f5307625f4ef4604a1c2e6fdad0
Amit Vemuri <136983+amitV@users.noreply.github.com>
2025-05-31 19:21:57 -0400

    connect 4 version w beckham profile pic

---
+++ beckham/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Connect4 from './Connect4';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Connect4 />
+    </div>
+  );
+}
+
+export default App;
